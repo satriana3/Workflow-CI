574e2d3f3410c9e75a2c376e3fa03496793ff9d8
Satriana <aaayna23@gmail.com>
2025-11-15 16:33:34 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -11,33 +11,29 @@ from sklearn.metrics import accuracy_score
 
 def main(data_path):
 
-    # Tidak memulai run baru jika MLflow sudah memulai run dari CLI
-    # Gunakan start_run(nested=True) untuk menghindari conflict
     with mlflow.start_run(nested=True):
 
         mlflow.autolog()
 
-        # Load dataset
         df = pd.read_csv(data_path)
 
         X = df.drop("math score", axis=1)
         y = df["math score"]
 
-        # Train test split
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2, random_state=42
         )
 
-        # Model
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
 
-        # Prediction
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
 
-        # Log metrics
         mlflow.log_metric("accuracy", acc)
+
+        # WAJIB UNTUK BUILD DOCKER MODEL
+        mlflow.sklearn.log_model(model, artifact_path="model")
 
         print(f"Model Accuracy: {acc}")
 
